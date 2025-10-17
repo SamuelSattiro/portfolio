0caea28687f4727e7599a60b2a760a0b48521de7
Samuel Sattiro <samuelsattiro.dev@gmail.com>
2025-10-17 13:02:54 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    pdding: 0;
+    border-sizing: border-box;
+  }
+`
+
+export default EstiloGlobal
